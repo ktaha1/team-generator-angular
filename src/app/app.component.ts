9f8d1fee8343c2b1c278bfafb8b459a2b5528763
Taha <itaha.kamil@gmail.com>
2022-02-24 22:20:46 +0100

    Add members

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'teamapp';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+
+  addMember() {
+    if (this.newMemberName) {
+      this.members.push(this.newMemberName);
+      this.newMemberName = '';
+      this.errorMessage = '';
+    } else {
+      this.errorMessage = 'Name cannot be empty';
+    }
+  }
+
+  typing(name: string) {
+    this.newMemberName = name;
+  }
 }
